e3269a5af03eddc2026185f0bde5b48368fb81bd
Kay Zheng <l04m33@gmail.com>
2013-08-01 16:32:04 +0800

    Remove server initialization code from Carthage app init code

--- src/carthage.erl
+++ src/carthage.erl
@@ -1,10 +1,16 @@
 -module(carthage).
 
--export([start/0]).
+-export([start/4]).
 
-start() ->
+start(LoginHandler, ClientHandler, ListenPort, NumAcceptors) ->
     ok = ensure_started(ranch),
-    ok = ensure_started(carthage).
+    ok = ensure_started(carthage),
+
+    ServerID = {LoginHandler, ClientHandler},
+    %% TODO: Start a new client supervisor
+    {ok, _} = ranch:start_listener(ServerID, NumAcceptors,
+            ranch_tcp, [{port, ListenPort}], carthage_login,
+            [{login_handler, LoginHandler}, {client_handler, ClientHandler}]).
 
 ensure_started(App) ->
     case application:start(App) of
--- src/carthage_app.erl
+++ src/carthage_app.erl
@@ -10,17 +10,7 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    case carthage_sup:start_link() of
-        {ok, _} = Ret ->
-            ListenPort = carthage_config:get(listening_port),
-            NumAcceptors = carthage_config:get(number_of_acceptors),
-            {ok, _} = ranch:start_listener(carthage_login, NumAcceptors,
-                    ranch_tcp, [{port, ListenPort}], carthage_login, []),
-            Ret;
-        Other ->
-            Other
-    end.
-
+    carthage_sup:start_link().
 
 stop(_State) ->
     ok.
