b767e5682f7fa7ccfad11bc470a18c3ed4f93186
Kay Zheng <l04m33@gmail.com>
2013-08-03 11:53:58 +0800

    Demo output

--- src/demo/demo_login.erl
+++ src/demo/demo_login.erl
@@ -2,6 +2,9 @@
 
 -export([init/2]).
 -export([network_message/2]).
+-export([socket_closed/1]).
+-export([socket_error/2]).
+-export([terminate/2]).
 
 init(InitReq, _Opts) ->
     carthage_req:send(InitReq, <<"Hello! ">>),
@@ -12,4 +15,16 @@ network_message(Req, _State) ->
     carthage_req:send(Req, Data),
     io:format("~p~n", [Data]),
     {done, Data}.
+
+socket_closed(State) ->
+    io:format("socket_closed~n"),
+    {ok, State}.
+
+socket_error(Error, State) ->
+    io:format("socket_error: ~p~n", [Error]),
+    {ok, State}.
+
+terminate(Reason, _State) ->
+    io:format("terminate: ~p~n", [Reason]),
+    ok.
 
